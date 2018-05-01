d98e4529f49c88c4ba23eccb95ce58492cbb56c4
ahmedouarzazi <infographia2000@yahoo.fr>
2018-05-01 22:35:58 +0200

    adding a project to test

---
+++ script.js
@@ -0,0 +1,30 @@
+$(function(){
+
+//  $('body').html("Hello ! This is my first script... <strong>Bouh !</strong>");
+
+
+
+//SELECTION D'ELEMENTS
+$('ul.bleu li').fadeOut("slow",function(){
+      $(this).fadeIn("slow");
+    });
+
+
+
+
+$('ul.rouge').css('background','red').css('color','yellow');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
